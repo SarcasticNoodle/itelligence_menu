7a387caaa0fe0b0135272b93425cb8b04c1f125f
TheDOme6 <dominik2003schulze@gmail.com>
2020-01-12 15:57:33 +0100

    extension tests

--- test/itelligence_menu_test.dart
+++ test/itelligence_menu_test.dart
@@ -1,5 +1,18 @@
 import 'package:test/test.dart';
+import 'package:itelligence_menu/src/extensions.dart';
 
 void main() {
-  
+  group('String extension tests', () {
+    test('SwapChar test', () {
+      var toSwap = 'Test String to Swap Chars';
+      expect(toSwap.swapChars(0, toSwap.length - 1), 'sest String to Swap CharT');
+    });
+
+    test('Reversed test', () {
+      var toReverse = 'test_string';
+      expect(toReverse.reversed, 'gnirts_tset');
+    });
+
+    
+  });
 }
